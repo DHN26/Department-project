550a7aa6169a32b6f40f6d035e95c337867e5b3f
divyashreehn26 <divyashreehn26@gmail.com>
2024-06-18 16:43:03 +0530

    Small update

--- Department/src/main/java/com/example/demo/entity/ErrorMessage.java
+++ Department/src/main/java/com/example/demo/entity/ErrorMessage.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor
 public class ErrorMessage {
 
-	
+	//this is a comment
 	private HttpStatus status;
 	private String message;
 	
